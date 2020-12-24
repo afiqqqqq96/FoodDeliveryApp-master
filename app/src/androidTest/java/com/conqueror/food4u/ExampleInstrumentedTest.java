895031e323b2e5c94d2878f2abf1756bd1358922
Muhammad Afiq BIn Mohd Murad <muhammadafiq99@users.noreply.github.com>
2020-12-24 05:51:56 -0800

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/conqueror/food4u/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/conqueror/food4u/ExampleInstrumentedTest.java
@@ -20,6 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
+        // Muaz gay
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.rajendra.foodapp", appContext.getPackageName());
